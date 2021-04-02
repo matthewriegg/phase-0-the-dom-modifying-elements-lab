61ce6e638efd7c951dc21bb4b2223002da97e222
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-04-01 17:59:14 -0700

    Deliverables

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>YOUR NAME is the champion</h1>";
+
+
+
